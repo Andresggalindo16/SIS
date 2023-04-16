c53f28d0163b82c08c5ed72b98a3d134bf07938c
Andresggalindo16 <andresggalindo1606@gmail.com>
2023-04-16 11:52:02 -0500

    comit xd

--- home.php
+++ home.php
@@ -7,13 +7,6 @@
 <div class="row">
   <div class="col-md-12">
     <?php echo display_msg($msg); ?>
-  </div>
- <div class="col-md-12">
-    <div class="panel">
-      <div class="jumbotron text-center">
-         <h1>Bienvenido a SIS</h1>     
-      </div>
-    </div>
- </div>
+  </div> 
 </div>
 <?php include_once('layouts/footer.php'); ?>
